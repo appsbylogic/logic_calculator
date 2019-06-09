47f525680216c9e43bae9c00a3e7ecff3f9ce45a
Fmaldonado6 <f_maldonado6@hotmail.com>
2019-06-08 20:00:33 -0500

    Notification

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -102,3 +102,46 @@ function updateCache(request, response) {
     return cache.put(request, response);
   });
 }
+function displayUpdateNotification() {
+
+  const link = document.createElement('a');
+  link.classList.add('update-notification');
+  link.setAttribute('href', '#');
+  link.innerHTML = 'Update is available. Click here to install.';
+ 
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.reload();
+  });
+ 
+  document.querySelector('body').appendChild(link);
+ }
+ function registerValidSW(swUrl) {
+  navigator.serviceWorker
+    .register(swUrl)
+    .then(registration => {
+      registration.onupdatefound = () => {
+        const installingWorker = registration.installing;
+        installingWorker.onstatechange = () => {
+          if (installingWorker.state === 'installed') {
+            if (navigator.serviceWorker.controller) {
+              // At this point, the old content will have been purged and
+              // the fresh content will have been added to the cache.
+              // It's the perfect time to display a "New content is
+              // available; please refresh." message in your web app.
+              console.log('New content is available; please refresh.');
+              displayUpdateNotification();
+            } else {
+              // At this point, everything has been precached.
+              // It's the perfect time to display a
+              // "Content is cached for offline use." message.
+              console.log('Content is cached for offline use.');
+            }
+          }
+        };
+      };
+    })
+    .catch(error => {
+      console.error('Error during service worker registration:', error);
+    });
+ }
